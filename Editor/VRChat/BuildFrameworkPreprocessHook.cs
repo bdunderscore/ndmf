501e001155811bf379a2b29ccb3fe6350b255edd
bd_ <bd_@nadena.dev>
2023-08-23 21:38:52 +0900

    split dependency processing up by phase

--- Editor/VRChat/BuildFrameworkPreprocessHook.cs
+++ Editor/VRChat/BuildFrameworkPreprocessHook.cs
@@ -1,18 +1,56 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
+using nadena.dev.build_framework.runtime;
 using UnityEngine;
 using VRC.SDKBase.Editor.BuildPipeline;
 
 namespace nadena.dev.build_framework.VRChat
 {
+    [AddComponentMenu("")]
+    internal class ContextHolder : MonoBehaviour
+    {
+        internal BuildContext context;
+    }
+    
     internal class BuildFrameworkPreprocessHook : IVRCSDKPreprocessAvatarCallback
     {
         public int callbackOrder => -5000;
         public bool OnPreprocessAvatar(GameObject avatarGameObject)
         {
+            if (avatarGameObject.GetComponent<AlreadyProcessedTag>()) return true;
+            
             try
             {
-                AvatarProcessor.ProcessAvatar(avatarGameObject);
+                var holder = avatarGameObject.AddComponent<ContextHolder>();
+                holder.hideFlags = HideFlags.DontSave;
+                holder.context = new BuildContext(avatarGameObject, AvatarProcessor.TemporaryAssetRoot);
+                
+                AvatarProcessor.ProcessAvatar(holder.context, BuiltInPhase.Resolving, BuiltInPhase.Transforming);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                return false;
+            }
+        }
+    }
+    
+    internal class BuildFrameworkOptimizeHook : IVRCSDKPreprocessAvatarCallback
+    {
+        public int callbackOrder => 0;
+        public bool OnPreprocessAvatar(GameObject avatarGameObject)
+        {
+            if (avatarGameObject.GetComponent<AlreadyProcessedTag>()) return true;
+
+            var holder = avatarGameObject.GetComponent<ContextHolder>();
+            if (holder == null) return true;
+            
+            try
+            {
+                AvatarProcessor.ProcessAvatar(holder.context, BuiltInPhase.Optimization, BuiltInPhase.Optimization);
+                holder.context.Finish();
+                UnityEngine.Object.DestroyImmediate(holder);
+                
                 return true;
             }
             catch (Exception e)
