b32cd77c631a62c97c88d9c3ed7d5c971a8809fd
bd_ <bd_@nadena.dev>
2023-08-26 16:46:39 +0900

    feat: add a sample plugin

---
+++ Packages/nadena.dev.av3-build-framework/Editor/Samples/SetViewpointPlugin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+using System.Collections.Generic;
+using nadena.dev.build_framework;
+using nadena.dev.build_framework.runtime.samples;
+using nadena.dev.build_framework.samples;
+using UnityEngine;
+
+[assembly: ExportsPlugin(typeof(SetViewpointPlugin))]
+
+namespace nadena.dev.build_framework.samples
+{
+    public class SetViewpointPlugin : Plugin
+    {
+        public override string QualifiedName => "nadena.dev.av3-build-framework.sample.set-viewpoint";
+
+        public override ImmutableList<PluginPass> Passes
+            => new List<PluginPass> {new SetViewpointPass()}.ToImmutableList();
+    }
+
+    public class SetViewpointPass : PluginPass
+    {
+        public override void Process(BuildContext context)
+        {
+            var obj = context.AvatarRootObject.GetComponentInChildren<SetViewpoint>();
+            if (obj != null)
+            {
+                context.AvatarDescriptor.ViewPosition =
+                    Quaternion.Inverse(context.AvatarRootTransform.rotation) * (
+                        obj.transform.position - context.AvatarRootTransform.position);
+            }
+        }
+    }
+}
