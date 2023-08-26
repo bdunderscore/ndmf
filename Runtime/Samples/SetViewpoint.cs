21c08ef2e9a837a4cbd1c50022664cd5a00c1da0
bd_ <bd_@nadena.dev>
2023-08-26 16:46:39 +0900

    feat: add a sample plugin

---
+++ Runtime/Samples/SetViewpoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using VRC.SDKBase;
+
+namespace nadena.dev.build_framework.runtime.samples
+{
+    public class SetViewpoint : MonoBehaviour, IEditorOnly
+    {
+        
+    }
+}
