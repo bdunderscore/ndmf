126ae740ee09eee3111b98d8df5de72041329180
bd_ <bd_@nadena.dev>
2023-09-10 22:35:47 +0900

    ci: wip

--- .github/ProjectRoot/build-docs.sh
+++ .github/ProjectRoot/build-docs.sh
@@ -1,8 +1,9 @@
-﻿#!/bin/bash
+﻿
 
 set -euxo pipefail
 
 apt-get -y update
 apt-get -y install dotnet7
-dotnet tool update -g docfx
+(cd /; dotnet tool update -g docfx)
 ls -l /root/.dotnet/tools
+ls -l .
