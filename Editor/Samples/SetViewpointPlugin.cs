ec31c941e33df343fad2f8b6e826c874ddff110f
bd_ <bd_@nadena.dev>
2023-09-10 17:34:35 +0900

    Add some doc-comments

--- Editor/Samples/SetViewpointPlugin.cs
+++ Editor/Samples/SetViewpointPlugin.cs
@@ -10,7 +10,17 @@ namespace nadena.dev.ndmf.sample
 {
     public class SetViewpointPlugin : Plugin<SetViewpointPlugin>
     {
+        /// <summary>
+        /// This name is used to identify the plugin internally, and can be used to declare BeforePlugin/AfterPlugin
+        /// dependencies. If not set, the full type name will be used.
+        /// </summary>
         public override string QualifiedName => "nadena.dev.av3-build-framework.sample.set-viewpoint";
+        
+        /// <summary>
+        /// The plugin name shown in debug UIs. If not set, the qualified name will be shown.
+        /// </summary>
+        public override string DisplayName => "Set viewpoint using object";
+
         protected override void Configure()
         {
             InPhase(BuildPhase.Transforming).Run("Set viewpoint", ctx =>
