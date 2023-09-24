f2d7d264c00adb095a953abb87d68708583dcba2
bd_ <bd_@nadena.dev>
2023-09-24 14:30:59 +0900

    fix: avatar names with non-path-safe characters break builds (#20)
    Fixes: #18

---
+++ UnitTests~/AvatarNameFilterTests.cs
@@ -0,0 +1,42 @@
+﻿using nadena.dev.ndmf;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    public class AvatarNameFilterTests
+    {
+        [Test]
+        public void TestAvatarNameFilter()
+        {
+            Assert.AreEqual(
+                "foo",
+                BuildContext.FilterAvatarName("foo")
+            );
+            
+            Assert.AreEqual(
+                "_con",
+                BuildContext.FilterAvatarName("con")
+            );
+            
+            Assert.AreEqual(
+                "_LPT4",
+                BuildContext.FilterAvatarName("LPT4")
+            );
+            
+            Assert.AreEqual(
+                "_AUX.avatar",
+                BuildContext.FilterAvatarName("AUX.avatar")
+            );
+            
+            Assert.AreEqual(
+                "foo_bar",
+                BuildContext.FilterAvatarName("foo/bar")
+            );
+            
+            Assert.AreEqual(
+                "foo_bar_baz_quux",
+                BuildContext.FilterAvatarName("foo\\bar?baz*quux")
+            );
+        }
+    }
+}
