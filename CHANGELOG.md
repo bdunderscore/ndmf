d725df959f0ef60b43028bf199410e74ced2099a
bd_ <bd_@nadena.dev>
2023-09-24 14:12:00 +0900

    feat: remove missing script components

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
+- Remove missing script components from the avatar at the start of the build.
+
 ### Changed
 
 ### Removed
---
+++ Editor/InternalPasses/RemoveMissingScriptComponents.cs
@@ -0,0 +1,21 @@
+﻿using nadena.dev.ndmf;
+using UnityEditor;
+using UnityEngine;
+
+namespace nadena.dev.ndmf.builtin
+{
+    /// <summary>
+    /// This pass removes all missing script components from the avatar. It will be run early in the Resolving phase; if
+    /// (for some reason) you need to run before this, declare a BeforePass constraint on this type.
+    /// </summary>
+    public sealed class RemoveMissingScriptComponents : Pass<RemoveMissingScriptComponents>
+    {
+        protected override void Execute(BuildContext context)
+        {
+            foreach (var child in context.AvatarRootObject.GetComponentsInChildren<Transform>(true))
+            {
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(child.gameObject);
+            }
+        }
+    }
+}
