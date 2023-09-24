eba9fb164f103979b71eca62919cb3983bcad371
bd_ <bd_@nadena.dev>
2023-09-24 14:12:00 +0900

    feat: remove missing script components

---
+++ Editor/InternalPasses/RemoveMissingScriptComponents.cs
@@ -0,0 +1,17 @@
+﻿using nadena.dev.ndmf;
+using UnityEditor;
+using UnityEngine;
+
+namespace nadena.dev.ndmf.builtin
+{
+    public sealed class RemoveMissingScriptComponents : Pass<RemoveMissingScriptComponents>
+    {
+        protected override void Execute(BuildContext context)
+        {
+            foreach (var child in context.AvatarRootObject.GetComponentsInChildren<Transform>(true))
+            {
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(child.gameObject);
+            }
+        }
+    }
+}
