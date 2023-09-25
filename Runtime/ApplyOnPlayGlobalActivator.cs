0b4e799f89679a83f8c19ab95d7e31d482ae45f7
anatawa12 <anatawa12@icloud.com>
2023-09-25 11:11:17 +0900

    fix: Skip ApplyOnPlay if Av3Emu is exists

--- Runtime/ApplyOnPlayGlobalActivator.cs
+++ Runtime/ApplyOnPlayGlobalActivator.cs
@@ -1,6 +1,7 @@
 ﻿#if UNITY_EDITOR
 
 using System;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
@@ -51,8 +52,6 @@ namespace nadena.dev.ndmf.runtime
         {
             if (!RuntimeUtil.IsPlaying || this == null) return;
 
-            var scene = gameObject.scene;
-
             // Check if Lyuma's Av3Emulator is present and enabled; if so, we leave preprocessing up to it.
             // First, find the type...
 
@@ -65,6 +64,8 @@ namespace nadena.dev.ndmf.runtime
 
             if (ty_av3emu != null)
             {
+                // find Av3Emulator for all scenes
+                foreach (var scene in Enumerable.Range(0, SceneManager.sceneCount).Select(SceneManager.GetSceneAt))
                 foreach (var root in scene.GetRootGameObjects())
                 {
                     if (root.GetComponentInChildren(ty_av3emu) != null)
@@ -74,7 +75,7 @@ namespace nadena.dev.ndmf.runtime
                 }
             }
 
-            foreach (var root in scene.GetRootGameObjects())
+            foreach (var root in gameObject.scene.GetRootGameObjects())
             {
                 foreach (var avatar in root.GetComponentsInChildren<VRCAvatarDescriptor>())
                 {
