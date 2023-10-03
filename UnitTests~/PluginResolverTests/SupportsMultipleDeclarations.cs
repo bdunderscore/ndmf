677de8f2e8695a9dc68f4397a79e3a82c97caa25
bd_ <bd_@nadena.dev>
2023-10-03 20:57:16 +0900

    feat: support multiple ExportsPlugin declarations

---
+++ UnitTests~/PluginResolverTests/SupportsMultipleDeclarations.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using nadena.dev.ndmf;
+using NUnit.Framework;
+using UnitTests.ExportsPluginTest;
+
+[assembly: ExportsPlugin(typeof(PluginA))]
+[assembly: ExportsPlugin(typeof(PluginB))]
+
+namespace UnitTests.ExportsPluginTest
+{
+    internal class PluginA : Plugin<PluginA>
+    {
+        protected override void Configure()
+        {
+            InPhase(BuildPhase.Transforming).Run("test", _ctx => { });
+        }
+    }
+    
+    internal class PluginB : Plugin<PluginB>
+    {
+        protected override void Configure()
+        {
+            InPhase(BuildPhase.Transforming).Run("test", _ctx => { });
+        }
+    }
+    
+    public class SupportsMultipleDeclarations
+    {
+        [Test]
+        public void TestSupportsMultipleDeclarations()
+        {
+            var resolver = new PluginResolver();
+            var plugins =
+                resolver.Passes.SelectMany(kv => kv.Item2) // passes per phase
+                    .Select(pass => pass.Plugin.GetType())
+                    .ToImmutableHashSet();
+            
+            Assert.IsTrue(plugins.Contains(typeof(PluginA)));
+            Assert.IsTrue(plugins.Contains(typeof(PluginB)));
+        }
+    }
+}
