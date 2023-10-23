82034674d69b1dff5d794d031a24ebd930b0c83a
Tayou <git@tayou.org>
2023-10-23 16:21:00 +0200

    add `NDMF` scripting define

---
+++ Editor/DefineSymbolsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEditor;
+
+namespace nadena.dev.ndmf {
+    [InitializeOnLoad]
+    public class DefineSymbolsManager {
+        private const string DefineName = "NDMF";
+
+        static DefineSymbolsManager()
+        {
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone).Split(';').ToList();
+            if (!defines.Contains(DefineName))
+            {
+                defines.Add(DefineName);
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, string.Join(";", defines));
+            }
+        }
+    }
+}
