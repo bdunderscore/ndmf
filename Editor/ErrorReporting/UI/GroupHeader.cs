dc13c52594f4fd92cb8207b1b77576dff5849e95
bd_ <bd_@nadena.dev>
2023-12-27 19:40:43 +0900

    fix: error group header is rendered incorrectly on Unity 2019.

--- Editor/ErrorReporting/UI/GroupHeader.cs
+++ Editor/ErrorReporting/UI/GroupHeader.cs
@@ -17,7 +17,7 @@ namespace nadena.dev.ndmf.ui
                 AssetDatabase.LoadAssetAtPath<StyleSheet>(
                     "Packages/nadena.dev.ndmf/Editor/ErrorReporting/UI/Resources/GroupHeader.uss");
             styleSheets.Add(styleSheet);
-            
+
             var titleElem = this.Q<Label>("group-title");
             var imageElem = this.Q<Image>("group-logo");
 
@@ -25,6 +25,16 @@ namespace nadena.dev.ndmf.ui
             {
                 imageElem.image = plugin.LogoTexture;
                 titleElem.style.display = DisplayStyle.None;
+
+#if !UNITY_2020_1_OR_NEWER
+                // Older versions of unity don't scale the image the way we want. Manually compute the size...
+                // Note that 2020.1 is a bit of a guess - I've only tried 2019.4.31f1 and 2022.3.6f1.
+                var aspectRatio = plugin.LogoTexture.width / (float)plugin.LogoTexture.height;
+                var height = 64 - 12;
+                var width = height * aspectRatio;
+                imageElem.style.width = width;
+                imageElem.style.height = height;
+#endif
             }
             else
             {
