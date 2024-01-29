386ade6ee625b1a3144ade0b787ddef181542a25
bd_ <bd_@nadena.dev>
2024-01-29 18:30:59 +0900

    feat: add non-component-based hook execution deduplication
    This is in preparation for coordination with VRCF:
    https://discord.com/channels/1001423276553285653/1001424777401077830/1201398914574712842

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [unreleased]
 
 ### Added
+- Added a non-component-based check for double execution of hooks (#142)
 
 ### Fixed
 
---
+++ Editor/VRChat/HookDedup.cs
@@ -0,0 +1,43 @@
+﻿#if NDMF_VRCSDK3_AVATARS
+
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace nadena.dev.ndmf.VRChat
+{
+    /// <summary>
+    /// Ensures that we don't run hooks more than once. This is in preparation for coordinating with VRCFury to have all
+    /// nondestructive utilities independently execute hooks as part of Apply on Play, while deduplicating to ensure that
+    /// we don't rerun hooks on the same avatar.
+    /// </summary>
+    internal static class HookDedup
+    {
+        internal class State
+        {
+            internal bool ranEarlyHook;
+            internal bool ranOptimization;
+        }
+
+        private static ConditionalWeakTable<GameObject, State> _avatars = new ConditionalWeakTable<GameObject, State>();
+        
+        public static State RecordAvatar(GameObject root)
+        {
+            if (_avatars.TryGetValue(root, out var state))
+            {
+                return state;
+            }
+
+            state = new State();
+            _avatars.Add(root, state);
+
+            return state;
+        }
+
+        public static bool HasAvatar(GameObject root)
+        {
+            return _avatars.TryGetValue(root, out _);
+        }
+    }
+}
+
+#endif
