2896716d1240f6fc5c3534171dd97b5472565c0c
bd_ <bd_@nadena.dev>
2024-02-08 21:04:02 +0900

    fix: don't force enable av3emu hook processing when apply on play is off (#160)

---
+++ Runtime/NonPersistentConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace nadena.dev.ndmf.config.runtime
+{
+    internal class NonPersistentConfig : ScriptableSingleton<NonPersistentConfig>
+    {
+        [SerializeField] public bool applyOnPlay = true;
+    }
+}
