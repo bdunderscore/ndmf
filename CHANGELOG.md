3126c4f4541730c49881cd8ba6d97e1506a36064
bd_ <bd_@nadena.dev>
2024-05-06 19:19:02 -0700

    fix: workaround VRCSDK bug where stale data is left in physbone state
    Closes: https://github.com/bdunderscore/modular-avatar/issues/845

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,21 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+- Workaround VRCSDK bug where stale PhysBones state could be retained over play mode transitions (#231) 
+
+### Security
+
 ## [1.4.0] - [2024-03-27]
 
 ### Added
---
+++ Editor/VRChat/ForceReinitPhysBonesHook.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using UnityEngine;
+using VRC.SDK3.Dynamics.PhysBone.Components;
+using VRC.SDKBase.Editor.BuildPipeline;
+
+#endregion
+
+namespace nadena.dev.ndmf.VRChat
+{
+    /// <summary>
+    /// When domain reload is disabled, the VRChat physbones gizmo can leave outdated data in the VRCPhysBone component
+    /// when entering play mode. Force reinitialize the components to work around this issue.
+    ///
+    /// Note that we only do this when entering play mode, as this is runtime state, not serialized state.
+    /// </summary>
+    public class ForceReinitPhysBonesHook : IVRCSDKPreprocessAvatarCallback
+    {
+        public int callbackOrder => int.MaxValue;
+
+        public bool OnPreprocessAvatar(GameObject avatarGameObject)
+        {
+            if (Application.isPlaying)
+            {
+                foreach (var physBone in avatarGameObject.GetComponentsInChildren<VRCPhysBone>(true))
+                {
+                    physBone.InitTransforms(true);
+                    physBone.InitParameters();
+                }
+            }
+
+            return true;
+        }
+    }
+}
