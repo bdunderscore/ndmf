e89b468674831ac0a2247300dc1d3c020aadfc1e
bd_ <bd_@nadena.dev>
2024-07-07 20:36:33 -0700

    fix: keyboard controls break due to Hierarchy harmony patching (#286)
    Fixes: #285

--- Editor/PreviewSystem/Harmony/HierarchyViewPatches.cs
+++ Editor/PreviewSystem/Harmony/HierarchyViewPatches.cs
@@ -2,10 +2,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using HarmonyLib;
 using JetBrains.Annotations;
+using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,14 +18,24 @@ using Object = UnityEngine.Object;
 
 namespace nadena.dev.ndmf.preview
 {
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     internal static class HierarchyViewPatches
     {
         private static readonly Type t_HierarchyProperty = AccessTools.TypeByName("UnityEditor.HierarchyProperty");
         private static readonly PropertyInfo p_pptrValue = AccessTools.Property(t_HierarchyProperty, "pptrValue");
+        private static readonly PropertyInfo p_rowIndex = AccessTools.Property(t_HierarchyProperty, "row");
 
         private static FieldInfo f_m_Rows; // List<TreeViewItem>
         private static FieldInfo f_m_RowCount; // int
+        private static FieldInfo f_m_SearchString; // string
         private static PropertyInfo p_objectPPTR;
+
+        /// <summary>
+        ///     For each GameObjectTreeViewDataSource, we maintain a virtual mapping of HierarchyProperties to
+        ///     row indexes. This is then used in GameObjectTreeViewDataSource.GetRow to return the row index,
+        ///     ignoring skipped rows.
+        /// </summary>
+        private static readonly ConditionalWeakTable<object, Dictionary<int, int>> _rowIndexRemap = new();
 
         internal static void Patch(Harmony h)
         {
@@ -36,6 +49,10 @@ namespace nadena.dev.ndmf.preview
                 BindingFlags.NonPublic | BindingFlags.Instance);
             f_m_RowCount =
                 t_GameObjectTreeViewDataSource.GetField("m_RowCount", BindingFlags.NonPublic | BindingFlags.Instance);
+            f_m_SearchString =
+                t_GameObjectTreeViewDataSource.GetField("m_SearchString",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+
             p_objectPPTR = t_GameObjectTreeViewItem.GetProperty("objectPPTR");
 
             var m_orig = AccessTools.Method(t_GameObjectTreeViewDataSource, "InitTreeViewItem",
@@ -59,18 +76,29 @@ namespace nadena.dev.ndmf.preview
 
             h.Patch(original: m_InitRows,
                 transpiler: new HarmonyMethod(m_transpiler),
-                postfix: new HarmonyMethod(AccessTools.Method(typeof(HierarchyViewPatches), "Postfix_InitializeRows")),
-                prefix: new HarmonyMethod(AccessTools.Method(typeof(HierarchyViewPatches), "Prefix_InitializeRows"))
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(HierarchyViewPatches), "Postfix_InitializeRows"))
             );
+
+            var m_GetRow = AccessTools.Method(t_GameObjectTreeViewDataSource, "GetRow");
+            h.Patch(m_GetRow,
+                postfix: new HarmonyMethod(AccessTools.Method(typeof(HierarchyViewPatches), nameof(Postfix_GetRow))));
         }
 
         private static int skipped = 0;
 
-        private static void Prefix_InitializeRows()
+        [UsedImplicitly]
+        private static void Postfix_GetRow(object __instance, int id, ref int __result)
         {
-            skipped = 0;
-        }
+            var cache = _rowIndexRemap.GetOrCreateValue(__instance);
+            var searchString = (string)f_m_SearchString.GetValue(__instance);
 
+            if (!string.IsNullOrEmpty(searchString))
+                // GetRow calls the underlying TreeValueDataSource routine which just loops through all rows (safely)
+                return;
+
+            if (cache.TryGetValue(__result, out var newResult)) __result = newResult;
+        }
+        
         [UsedImplicitly]
         private static void Postfix_InitializeRows(object __instance)
         {
@@ -102,9 +130,14 @@ namespace nadena.dev.ndmf.preview
             ILGenerator generator)
         {
             var m_shouldLoop = AccessTools.Method(typeof(HierarchyViewPatches), "ShouldLoop");
-
             var m_Next = AccessTools.Method(t_HierarchyProperty, "Next", new[] { typeof(int[]) });
 
+            var cache_arg = generator.DeclareLocal(typeof(Dictionary<int, int>));
+            yield return new CodeInstruction(OpCodes.Ldarg_0); // this
+            yield return new CodeInstruction(OpCodes.Call,
+                AccessTools.Method(typeof(HierarchyViewPatches), nameof(InitializeRows_Entry)));
+            yield return new CodeInstruction(OpCodes.Stloc, cache_arg);
+            
             foreach (var c in instructions)
             {
                 if (c.Is(OpCodes.Callvirt, m_Next))
@@ -119,11 +152,12 @@ namespace nadena.dev.ndmf.preview
                     var tmp = new CodeInstruction(OpCodes.Ldloc, stash_obj);
                     tmp.labels.Add(loopLabel);
                     yield return tmp;
-
+                    
                     yield return new CodeInstruction(OpCodes.Ldloc, stash_arg);
                     yield return new CodeInstruction(OpCodes.Call, m_Next);
 
                     // Check if this item should be ignored.
+                    yield return new CodeInstruction(OpCodes.Ldloc, cache_arg);
                     yield return new CodeInstruction(OpCodes.Ldloc, stash_obj);
                     yield return new CodeInstruction(OpCodes.Call, m_shouldLoop);
                     yield return new CodeInstruction(OpCodes.Brtrue_S, loopLabel);
@@ -136,13 +170,26 @@ namespace nadena.dev.ndmf.preview
         }
 
         [UsedImplicitly]
-        private static bool ShouldLoop(object hierarchyProperty)
+        private static Dictionary<int, int> InitializeRows_Entry(object self)
+        {
+            skipped = 0;
+            var rowCache = _rowIndexRemap.GetOrCreateValue(self);
+            rowCache.Clear();
+
+            return rowCache;
+        }
+
+        [UsedImplicitly]
+        private static bool ShouldLoop(Dictionary<int, int> rowIndexCache, HierarchyProperty hierarchyProperty)
         {
             var sess = PreviewSession.Current;
             if (sess == null) return false;
 
             if (hierarchyProperty == null) return false;
 
+            var rowIndex = (int)p_rowIndex.GetValue(hierarchyProperty);
+            rowIndexCache[rowIndex] = rowIndex - skipped;
+            
             var pptrValue = p_pptrValue.GetValue(hierarchyProperty);
             if (pptrValue == null) return false;
 
@@ -152,6 +199,7 @@ namespace nadena.dev.ndmf.preview
             return skip;
         }
 
+        [SuppressMessage("ReSharper", "UnusedParameter.Local")]
         private static bool Prefix_InitTreeViewItem(
             object __instance,
             ref object item,
