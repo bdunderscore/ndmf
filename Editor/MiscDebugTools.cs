8d02bedaf6cbfb7a8413eec45de8c5eb586f07a8
bd_ <bd_@nadena.dev>
2024-08-18 13:14:52 -0700

    chore: add some secret debug tools

--- Editor/MiscDebugTools.cs
+++ Editor/MiscDebugTools.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,7 @@ namespace nadena.dev.ndmf.ui
 {
     internal static class MiscDebugTools
     {
+        #if NDMF_DEBUG
         [MenuItem("Tools/NDM Framework/Debug Tools/Dump Scenes")]
         public static void DumpScenes()
         {
@@ -29,11 +31,47 @@ namespace nadena.dev.ndmf.ui
             }
         }
         
+        [MenuItem("Tools/NDM Framework/Debug Tools/Dump Prefab Stage Objects (recursive)")]
+        public static void DumpPrefabStageObjects()
+        {
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            if (prefabStage == null)
+            {
+                Debug.Log("No prefab stage active.");
+                return;
+            }
+ 
+            Debug.Log($"Prefab stage {prefabStage.name}:");
+            DumpPrefabStageObjectsRecursive(prefabStage.prefabContentsRoot, indent: 0);
+
+            void DumpPrefabStageObjectsRecursive(GameObject obj, int indent)
+            {
+                Debug.Log(new string(' ', indent * 2) + obj.name);
+                foreach (Transform child in obj.transform)
+                {
+                    DumpPrefabStageObjectsRecursive(child.gameObject, indent + 1);
+                }
+            }
+        }
+        
+        [MenuItem("Tools/NDM Framework/Debug Tools/Create GameObject at root")]
+        public static void CreateGameObjectAtRoot()
+        {
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            var scene = prefabStage?.scene ?? SceneManager.GetActiveScene();
+            
+            var go = new GameObject("New GameObject");
+            SceneManager.MoveGameObjectToScene(go, scene);
+
+            go.transform.SetParent(null, worldPositionStays: true);
+        }
+        
         [MenuItem("Tools/NDM Framework/Debug Tools/Reload Domain")]
         public static void ReloadDomain()
         {
             Debug.Log("Reloading domain...");
             UnityEditor.EditorUtility.RequestScriptReload();
         }
+        #endif
     }
 }
