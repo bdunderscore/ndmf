69bc0dfeece473becc115a8d925c030afb54a93a
bd_ <bd_@nadena.dev>
2024-11-11 20:14:11 -0800

    Add support for synced layer overrides

---
+++ UnitTests~/AnimationServices/SyncedLayerOverrideAccessTest.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using nadena.dev.ndmf.animator;
+using nadena.dev.ndmf.UnitTestSupport;
+using NUnit.Framework;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace UnitTests.AnimationServices
+{
+    public class SyncedLayerOverrideAccessTest : TestBase
+    {
+        [Test]
+        public void Test_ExtractStateMotionPairs()
+        {
+            var ac = CreateTestController(out var clip1, out var clip2, out var s1);
+
+            var l1 = ac.layers[1];
+            l1.SetOverrideMotion(s1, clip1);
+            ac.layers = new[]
+            {
+                ac.layers[0],
+                l1
+            };
+            
+            var pairs = SyncedLayerOverrideAccess.ExtractStateMotionPairs(ac.layers[1]).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            
+            Assert.AreEqual(1, pairs.Count);
+            Assert.AreEqual(clip1, pairs[s1]);
+        }
+        
+        [Test]
+        public void Test_ExtractStateBehaviorPairs()
+        {
+            var ac = CreateTestController(out var clip1, out var clip2, out var s1);
+
+            var l1 = ac.layers[1];
+            l1.SetOverrideBehaviours(s1, new StateMachineBehaviour[] { ScriptableObject.CreateInstance<TestStateBehavior>() });
+            ac.layers = new[]
+            {
+                ac.layers[0],
+                l1
+            };
+            
+            var pairs = SyncedLayerOverrideAccess.ExtractStateBehaviourPairs(ac.layers[1]).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            
+            Assert.AreEqual(1, pairs.Count);
+            Assert.AreEqual(1, pairs[s1].Length);
+            Assert.AreEqual(typeof(TestStateBehavior), pairs[s1][0].GetType());
+        }
+
+
+        [Test]
+        public void Test_SetStateMotionPairs()
+        {
+            var ac = CreateTestController(out var clip1, out var clip2, out var s1);
+
+            var l1 = ac.layers[1];
+            SyncedLayerOverrideAccess.SetStateMotionPairs(l1, new Dictionary<AnimatorState, Motion>
+            {
+                {s1, clip2}
+            });
+            
+            // Make sure we can save back to the controller (Unity native code) and read back
+            ac.layers = new[]
+            {
+                ac.layers[0],
+                l1
+            };
+            l1 = ac.layers[1];
+            
+            Assert.AreEqual(clip2, l1.GetOverrideMotion(s1));
+        }
+
+        [Test]
+        public void Test_SetStateBehaviourPairs()
+        {
+            var ac = CreateTestController(out var clip1, out var clip2, out var s1);
+            
+            var l1 = ac.layers[1];
+            SyncedLayerOverrideAccess.SetStateBehaviourPairs(l1, new Dictionary<AnimatorState, ScriptableObject[]>
+            {
+                {s1, new ScriptableObject[] {ScriptableObject.CreateInstance<TestStateBehavior>()}}
+            });
+            
+            // Make sure we can save back to the controller (Unity native code) and read back
+            ac.layers = new[]
+            {
+                ac.layers[0],
+                l1
+            };
+            l1 = ac.layers[1];
+            
+            Assert.AreEqual(1, l1.GetOverrideBehaviours(s1).Length);
+        }
+        
+
+        private AnimatorController CreateTestController(out AnimationClip clip1, out AnimationClip clip2, out AnimatorState s1)
+        {
+            var ac = TrackObject(new AnimatorController());
+            var sm = TrackObject(new AnimatorStateMachine());
+            ac.layers = new[]
+            {
+                new AnimatorControllerLayer {stateMachine = sm},
+                new AnimatorControllerLayer {syncedLayerIndex = 0}
+            };
+            
+            clip1 = TrackObject(new AnimationClip {name = "c1"});
+            clip2 = TrackObject(new AnimationClip {name = "c2"});
+            
+            s1 = TrackObject(new AnimatorState {name = "s1", motion = clip1});
+            var s2 = TrackObject(new AnimatorState {name = "s2", motion = clip2});
+            
+            sm.states = new[]
+            {
+                new ChildAnimatorState {state = s1},
+                new ChildAnimatorState {state = s2}
+            };
+            return ac;
+        }
+    }
+}
--- UnitTests~/AnimationServices/VirtualLayerTest.cs
+++ UnitTests~/AnimationServices/VirtualLayerTest.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
 using nadena.dev.ndmf.animator;
+using nadena.dev.ndmf.UnitTestSupport;
 using NUnit.Framework;
 using UnityEditor.Animations;
+using UnityEngine;
 
 namespace UnitTests.AnimationServices
 {
@@ -120,6 +124,63 @@ namespace UnitTests.AnimationServices
                 state => Assert.AreEqual("x", state.stateMachine.name),
                 state => Assert.AreEqual("x", state.StateMachine.Name)
             );
+        }
+
+        [Test]
+        public void SyncedLayerOverridesArePreserved()
+        {
+            var testController = LoadAsset<AnimatorController>("TestAssets/SyncedLayers.controller");
+            var context = new CloneContext(new GenericPlatformAnimatorBindings());
+            var virtualController = context.Clone(testController);
+            
+            var commitContext = new CommitContext();
+            var committed = commitContext.CommitObject(virtualController);
+            
+            var baseLayer = committed.layers[0];
+            var c1 = baseLayer.stateMachine.states.First(s => s.state.name == "c1").state;
+            var behavior = baseLayer.stateMachine.states.First(s => s.state.name == "behavior").state;
+
+            var syncedLayer = committed.layers[1];
+            Assert.AreEqual("c2", syncedLayer.GetOverrideMotion(c1).name);
+            Assert.AreEqual(1, syncedLayer.GetOverrideBehaviours(behavior).Length);
+            Assert.AreEqual(typeof(TestStateBehavior), syncedLayer.GetOverrideBehaviours(behavior)[0].GetType());
+        }
+        
+        [Test]
+        public void SyncedLayerOverridesCanBeChanged()
+        {
+            var testController = LoadAsset<AnimatorController>("TestAssets/SyncedLayers.controller");
+            var c3 = LoadAsset<AnimationClip>("TestAssets/c3.anim");
+            var context = new CloneContext(new GenericPlatformAnimatorBindings());
+            var virtualController = context.Clone(testController);
+
+            var virtBaseLayer = virtualController.Layers.ToList()[0];
+            var v_c1 = virtBaseLayer.StateMachine.States.First(s => s.State.Name == "c1").State;
+            var v_behavior = virtBaseLayer.StateMachine.States.First(s => s.State.Name == "behavior").State;
+            
+            var virtLayer = virtualController.Layers.ToList()[1];
+            using (new AssertInvalidate(virtLayer))
+            {
+                virtLayer.SyncedLayerMotionOverrides =
+                    ImmutableDictionary<VirtualState, VirtualMotion>.Empty.Add(v_c1, context.Clone(c3));
+            }
+            using (new AssertInvalidate(virtLayer))
+            {
+                virtLayer.SyncedLayerBehaviourOverrides = ImmutableDictionary<VirtualState, ImmutableList<StateMachineBehaviour>>.Empty
+                        .Add(v_behavior, ImmutableList<StateMachineBehaviour>.Empty.Add(ScriptableObject.CreateInstance<TestStateBehavior1>()));
+            }
+            
+            var commitContext = new CommitContext();
+            var committed = commitContext.CommitObject(virtualController);
+            
+            var baseLayer = committed.layers[0];
+            var c1 = baseLayer.stateMachine.states.First(s => s.state.name == "c1").state;
+            var behavior = baseLayer.stateMachine.states.First(s => s.state.name == "behavior").state;
+
+            var syncedLayer = committed.layers[1];
+            Assert.AreEqual("c3", syncedLayer.GetOverrideMotion(c1).name);
+            Assert.AreEqual(1, syncedLayer.GetOverrideBehaviours(behavior).Length);
+            Assert.AreEqual(typeof(TestStateBehavior1), syncedLayer.GetOverrideBehaviours(behavior)[0].GetType());
         }
     }
 }
