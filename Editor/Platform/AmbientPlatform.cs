3ce264c893932f5889ba6abe8a246d89a3282606
bd_ <bd_@nadena.dev>
2024-12-02 19:33:36 -0800

    wip: avatar platform API

---
+++ Editor/Platform/AmbientPlatform.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+
+namespace nadena.dev.ndmf.platform
+{
+    internal static class AmbientPlatform
+    {
+        /// <summary>
+        ///     The default platform to use in legacy calls like AvatarProcessor.ProcessAvatar
+        /// </summary>
+        public static INDMFPlatformProvider DefaultPlatform { get; internal set; } = new GenericPlatform();
+    }
+}
