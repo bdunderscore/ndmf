10aca630dc9f662c9119ed8aac64589d78d32460
bd_ <bd_@nadena.dev>
2025-04-02 19:00:28 -0700

    fix(BREAKING): Correct namespace for IVirtualizedMotion

--- Editor/API/AnimatorServices/VirtualControllerContext.cs
+++ Editor/API/AnimatorServices/VirtualControllerContext.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
-using API;
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEditor.Animations;
