a87636529ae2ed1a4c9c169143c07dddbfdabd86
ColorlessColor <171957320+ColorlessColor@users.noreply.github.com>
2025-04-28 09:36:00 +0800

    fix: forgot to load the l10n assets for zh-hans (#607)
    * fix: forgot to load the l10n assets for zh-hans
    
    * chore: update CHANGELOG

--- Editor/UI/Localization/NDMFLocales.cs
+++ Editor/UI/Localization/NDMFLocales.cs
@@ -10,12 +10,19 @@ namespace nadena.dev.ndmf.localization
             "en-US",
             () => new List<LocalizationAsset>()
             {
+                // en-US.po
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(
                     AssetDatabase.GUIDToAssetPath("5cb11a9adc5d7404d8c01d558a5c0af6")
                 ),
+                // ja-JP.po
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(
                     AssetDatabase.GUIDToAssetPath("87c99a0330751d842a030f1385973541")
                 ),
+                // zh-Hans.po
+                AssetDatabase.LoadAssetAtPath<LocalizationAsset>(
+                    AssetDatabase.GUIDToAssetPath("6916b2591b094f87a5d0fff8ae0b2186")
+                ),
+                // zh-Hant.po
                 AssetDatabase.LoadAssetAtPath<LocalizationAsset>(
                     AssetDatabase.GUIDToAssetPath("b1fe4225ad3686e46bb3257770364b6e")
                 )                
